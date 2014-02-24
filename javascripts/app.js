7fb53b3721c3edd632f93f014faa39e80e263560
dbushy727 <d.bushkanets@gmail.com>
2014-02-23 22:10:52 -0500

    added links

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,18 @@
-
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
 
 
 
 
 $(function() {
-
+  
   var parallax = (function() {
       'use strict';
 
