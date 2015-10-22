87562c18dc1c5dd3884e3c6ad6e46b2fa02c36f0
dbushkanets <dbushkanets@harryfox.com>
2015-10-21 23:05:57 -0400

    added all images to cdn

--- js/projects.js
+++ js/projects.js
@@ -2,9 +2,9 @@ var projects = {
     "bagel_works": {
         "title": "Bagel Works",
         "subtitle": "Bagel Shop in Huntington, NY",
-        "image_1": "img/bagelworks/bagelworks_computer.png",
-        "image_2": "img/bagelworks/menu.png",
-        "image_3": "img/bagelworks/contact.png",
+        "image_1": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482397/bagelworks_computer_crfutb.png",
+        "image_2": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482397/menu_ozn5uq.png",
+        "image_3": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482396/contact_oxypsz.png",
         "description": "<p><a target=\"_BLANK\" href=\"http://dbushy727.github.io/bagelworks/theme/homepage.html\">Bagel Works</a> is an informational website designed for a small neighborhood bagel shop in Huntington, New York. The owners wanted a simple one-page website that was very clean, simple and easy to find important information (ie - menu items, catering deals, pricing, location, hours, etc.)</p>",
         "languages": [
             "HTML",
@@ -19,9 +19,9 @@ var projects = {
     "defined": {
         "title": "Defined",
         "subtitle": "Digital Fitness Tracking Notebook",
-        "image_1": "img/defined/workout_screen_computer.png",
-        "image_2": "img/defined/history.png",
-        "image_3": "img/defined/performance.png",
+        "image_1": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482587/workout_screen_computer_xdhpjn.png",
+        "image_2": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482586/history_i1lt5i.png",
+        "image_3": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482586/performance_mpik3u.png",
         "description": "<p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>",
         "languages": [
             "Ruby",
@@ -41,9 +41,9 @@ var projects = {
     "gh_discovery": {
         "title": "Github Discovery",
         "subtitle": "Explore The World's Largest Source, Github",
-        "image_1": "img/github_discovery/hello_world_computer.png",
-        "image_2": "img/github_discovery/trending.png",
-        "image_3": "img/github_discovery/categories.png",
+        "image_1": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482707/hello_world_computer_rw2qzb.png",
+        "image_2": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482706/trending_kvfao8.png",
+        "image_3": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482707/categories_tnr94h.png",
         "description": "<p>A discovery application based off the Github API, <a href=\"http://github-discovery.herokuapp.com\" target=\"_BLANK\">Github Discovery</a> allows users to browse or search for their favorite categories, technologies, languages, and buzz words.</p><p>Investigate new ways to collaborate, innovate, or draw inspiration from the world's largest, Github.</p>",
         "languages": [
             "Ruby",
@@ -62,9 +62,9 @@ var projects = {
     "mom": {
         "title": "Mom",
         "subtitle": "Virutal Over-Protective Mom",
-        "image_1": "img/mom/mom_computer.png",
-        "image_2": "img/mom/eat.png",
-        "image_3": "img/mom/weather.png",
+        "image_1": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482717/mom_computer_edgfzy.png",
+        "image_2": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482716/eat_jwjuvi.png",
+        "image_3": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482717/weather_fbjoop.png",
         "description": "<p>Mom is a reminder service that acts as your personal virtual mom. When you sign up for Mom, you will receive text message reminders for everyday important items that mom can't help but remind you about.</p> <p>Every morning, Mom will deliver the day's weather report as well as your local train status. Mom will also text you some lunch recommendations at noon based on your registered location.</p> <p>Mom uses the Yelp API for food, Weather API for weather, scrapes MTA open data for train status, and uses the Twilio API to send texts.</p>", 
         "languages": [
             "Ruby",
@@ -82,9 +82,9 @@ var projects = {
     "bullish": {
         "title": "Bullish",
         "subtitle": "Stock Tracking Algorithms, Made Easy.",
-        "image_1": "img/stock/stock_computer.png",
-        "image_2": "img/stock/intraday.png",
-        "image_3": "img/stock/testcase.png",
+        "image_1": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482964/stock_computer_vwjtc4.png",
+        "image_2": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482965/intraday_wzxj4b.png",
+        "image_3": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445482964/testcase_ocdkai.png",
         "description": "<p>Bullish is a stock reasearch tool that allows users to create custom 'Test Cases' that will be run against historical data to validate or inspire trading hypotheses.</p> <p>Bullish presents the user with a large set of data all on one screen. Components include: price, volume, stochastic formulas, intraday charts, change data, averages, etc.</p> <p>All of the visualizations have been custom made from scratch using the D3.js library. This includes: custom candlesticks, scales, sequencing, grids and more.</p>",
         "languages": [
             "PHP",
@@ -103,9 +103,9 @@ var projects = {
     "commandcenter": {
         "title": "Command Center",
         "subtitle": "Rights Management Platform",
-        "image_1": "img/cc/computer_home.png",
-        "image_2": "img/cc/tools.png",
-        "image_3": "img/cc/song.png",
+        "image_1": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445483072/computer_home_zjszn2.png",
+        "image_2": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445483069/tools_eu5qu3.png",
+        "image_3": "http://res.cloudinary.com/dpjzmn026/image/upload/v1445483069/song_ryqpy4.png",
         "description": "<p>Command Center is the platform that allows music rights-holders to track, maintain, and manage their assets, as well as offer a suite of services geared towards rights administration.</p> <p>Command Center has two different sides. The Public facing side and the Admin facing side.</p> <p>Command Center is an ongoing project that is currently owned by HFA.</p>",
         "languages": [
             "PHP",
